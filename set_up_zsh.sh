180ead81cacb99c69b9073c2585cb92c47a805bb
Yaozhong Kang <96532971+york1to@users.noreply.github.com>
2023-11-13 02:32:27 +0800

    Create set_up_zsh.sh

---
+++ set_up_zsh.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update package list and install Zsh
+echo "Installing Zsh..."
+sudo apt-get update
+sudo apt-get install -y zsh
+
+# Set Zsh as the default shell
+chsh -s $(which zsh)
+
+# Install Oh My Zsh
+echo "Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zplug (a plugin manager for Zsh)
+echo "Installing zplug..."
+export ZPLUG_HOME=~/.zplug
+git clone https://github.com/zplug/zplug $ZPLUG_HOME
+
+# Add zplug to .zshrc
+echo "source $ZPLUG_HOME/init.zsh" >> ~/.zshrc
+
+# Install Pure prompt
+echo "Installing Pure prompt..."
+zplug "sindresorhus/pure", as:theme, use:pure.zsh
+
+# Install zsh-autosuggestions
+echo "Installing zsh-autosuggestions..."
+zplug "zsh-users/zsh-autosuggestions"
+
+# Install zsh-syntax-highlighting
+echo "Installing zsh-syntax-highlighting..."
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+# Source zplug and install plugins
+echo "source $ZPLUG_HOME/init.zsh
+zplug install
+zplug load" >> ~/.zshrc
+
+# Reload Zsh configuration
+echo "Reloading Zsh configuration..."
+zsh -i -c 'source ~/.zshrc'
+
+echo "Installation complete. Please restart your terminal."
